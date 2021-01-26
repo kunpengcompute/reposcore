7a69fffed4c2a7652518f767d0884c90396bd43c
Yikun Jiang <yikunkero@gmail.com>
2021-01-26 17:46:44 +0800

    Add update failed retry

--- reposcore/utils/matrix.py
+++ reposcore/utils/matrix.py
@@ -1,12 +1,3 @@
-# This list contains the project that `git` tool is broken.
-BROKEN_PROJECT_MAPPING = {
-    'openstack/openstack': [
-        'openstack-tempest-skiplist',
-        'whitebox-tempest-plugin',
-        'xstatic-graphlib']
-}
-
-
 # TODO(yikun): language file extension map should be more complete
 LANGUAGE_MAPPING = {
     "C": ['c', 'h'],
