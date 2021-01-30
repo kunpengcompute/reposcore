cc8e085de538e3b56659de3090229d190130ce74
Yikun Jiang <yikunkero@gmail.com>
2021-01-30 14:22:08 +0800

    Improve the page stable and user experience (#52)
    1. 10s to wait
    2. friendly note
    3. exception process

--- web/app.py
+++ web/app.py
@@ -10,14 +10,22 @@ rs = SingleRepoScore("../etc/reposcore.conf")
 @app.route('/', methods=['GET', 'POST'])
 def login():
     output = {}
+    query_ok = True
+    repo_url = ''
     if request.method == 'POST':
-        repo_url = request.form['reponame']
-        output = rs.get_score(repo_url)
+        try:
+            repo_url = request.form['reponame']
+            output = rs.get_score(repo_url)
+            print("OK: %s" % repo_url)
+        except Exception:
+            print("Failed: %s" % repo_url)
+            query_ok = False
 
     return render_template(
         'index.html',
-        name=output.get('name'),
-        url=output.get('url'),
-        language=output.get('language'),
-        score=output.get('score')
+        query_ok=query_ok,
+        name=output.get('name', ''),
+        url=output.get('url', repo_url),
+        language=output.get('language', '无'),
+        score=output.get('criticality_score')
     )
